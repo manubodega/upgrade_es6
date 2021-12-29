098c6393794378074e2a3cf30b92a5c3f7f2a9f1
manubodega <manuel.palomeque@bootcamp-upgrade.com>
2021-12-29 19:44:14 +0100

    iteracion#2

---
+++ iteracion2.js
@@ -0,0 +1,33 @@
+const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
+let {title, gender} = game
+console.log(title)
+console.log(gender)
+
+
+const fruits = ['Banana', 'Strawberry', 'Orange'];
+let fruit1 = fruits[0]
+let fruit2 = fruits[1]
+let fruit3 = fruits[2]
+console.log(fruit1)
+console.log(fruit2)
+console.log(fruit3)
+
+
+const animalFunction = () => {
+    return {name: 'Bengal Tiger', race: 'Tiger'}
+};
+let _name = animalFunction().name
+let _race = animalFunction().race
+console.log(_name)
+console.log(_race)
+
+
+const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+let {name, itv} = car
+console.log(name)
+let itv1 = itv[0]
+let itv2 = itv[1]
+let itv3 = itv[2]
+console.log(itv1)
+console.log(itv2)
+console.log(itv3)
